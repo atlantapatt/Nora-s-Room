be13a7a8909543b7439ea5bff5139511da3c1603
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-09-08 15:44:20 -0700

    started ItemCard functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import ClothesCard from './components/ClothesCard';
+import ItemCard from './components/ItemCard';
 import Navbar from './components/NavBar';
 import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom'
 
 function App() {
   const [clothes, setClothes] = useState([])
   const [category, setCategory] = useState('all')
-  
+  const [location, setLocation] = useState('all')
+  const [url, setUrl] = useState('')
+  const [ratings, setRatings] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:9292/clothes")
@@ -15,11 +19,27 @@ function App() {
     .then((data) => setClothes(data))
   },[])
 
+  
+    
+  useEffect(() => {
+      fetch("http://localhost:9292/reviews")
+      .then((r) => r.json())
+      .then((data) => setRatings(data))
+    },[])
+
   console.log(clothes)
 
 
+let clothesLocation = clothes.filter(clothes => {
+  if (location == 'all') {
+    return clothes
+  } else
+    return clothes.store_id == location
+})
 
-  let clothesCategory = clothes.filter(clothes => {
+console.log(clothesLocation)
+
+  let clothesCategory = clothesLocation.filter(clothes => {
     if (category == 'all') {
       return clothes
     } else
@@ -29,18 +49,22 @@ function App() {
 
 
   const listofClothes = clothesCategory.map((data) => {
-    return <ClothesCard data={data} />
+    return <ClothesCard data={data} url={url} setUrl={setUrl} ratings={ratings} />
   })
 
-
+console.log(listofClothes)
   
   return (
-    <div className="App">
-      <Navbar  setCategory={setCategory}/>
-      <div className='list_clothes'>
-        {listofClothes}
-      </div>
-    </div>
+        <Router>
+          <div className='App'>
+          <Navbar setLocation={setLocation}  setCategory={setCategory} />
+          
+          <Switch>
+            <Route exact path='/' element={listofClothes}></Route>
+            <Route exact path={url} element={<ItemCard clothes={clothes} url={url} ratings={ratings}/>}></Route>
+          </Switch>
+          </div>
+        </Router>
   );
 }
 
